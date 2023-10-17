dab215b088d81768b393d380611ececa63812999
kotou@actynetwork <maikotou@MainoiMac.local>
2023-10-17 13:25:30 +0900

    topImage/svghover修正

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,14 +1,17 @@
 export default function slider() {
   var windowwidth = window.innerWidth || document.documentElement.clientWidth || 0;
-  if (windowwidth > 768){
-    var responsiveImage = [//PC用の画像
+  if (windowwidth > 768) {
+
+    //PC用の画像
+    var responsiveImage = [
       { src: '//localhost:3000/wp-content/themes/my_theme/images/slide/mainview_01.jpg' },
       { src: '//localhost:3000/wp-content/themes/my_theme/images/slide/mainview_02.jpg' },
       { src: '//localhost:3000/wp-content/themes/my_theme/images/slide/mainview_03.jpg' },
       { src: '//localhost:3000/wp-content/themes/my_theme/images/slide/mainview_04.jpg'},
     ];
   } else {
-    var responsiveImage = [//タブレットサイズ（768px）以下用の画像
+    //タブレットサイズ（768px）以下用の画像
+    var responsiveImage = [
     { src: '//localhost:3000/wp-content/themes/my_theme/images/slide/sp-mainview_01.jpg' },
     { src: '//localhost:3000/wp-content/themes/my_theme/images/slide/sp-mainview_02.jpg' },
     { src: '//localhost:3000/wp-content/themes/my_theme/images/slide/sp-mainview_03.jpg' },
@@ -16,17 +19,20 @@ export default function slider() {
     ];
   }
 
-//Vegas全体の設定
-
+  //Vegas全体の設定
   jQuery(function ($) {
     $('#js-vegas').vegas({
-      overlay: false,//画像の上に網線やドットのオーバーレイパターン画像を指定。
+
+    /* option
+    https://tetsu029.web.fc2.com/vegas.html*/
+      overlay: false,
       transition: 'blur2',
-      transitionDuration: 2000,//切り替わりのアニメーション時間をミリ秒単位で設定
-      delay: 5000,//スライド間の遅延をミリ秒単位で。
+      transitionDuration: 2000,
+      delay: 5000,
       animationDuration: 20000,
-      slides: responsiveImage,//画像設定を読む
+      slides: responsiveImage,
       cover: true,
+      align: top,
     });
   });
 };
